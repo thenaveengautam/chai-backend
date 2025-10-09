eecc7cc06e06cfbb5baf11d45c8bc09077b05c11
thenaveengautam <dmnaveengautam@gmail.com>
2025-10-09 23:41:07 +0530

    all setup configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,48 +9,49 @@ const userSchema = new Schema({
         unique: true,
         lowercase: true,
         trim: true,
-        index: true
+        index: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
     },
     fullName: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
     },
     avatar: {
         type: String, // cloudnary url
-        required: true
+        required: true,
     },
     coverImage: {
         type: String, // cloudnary url
     },
     watchHistory: {
         type: Schema.Types.ObjectId,
-        ref: "Video"
+        ref: "Video",
     },
     password: {
         type: String,
-        required: [true, 'password is required']
+        required: [true, "password is required"],
     },
     refreshToken: {
-        type: String
-    }
-}, { timestamps: true })
+        type: String,
+    },
+}, { timestamps: true }
+)
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+    if (!this.isModified("password")) return next()
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
+    return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = function () {
@@ -59,23 +60,23 @@ userSchema.methods.generateAccessToken = function () {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName
+            fullName: this.fullName,
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
         }
     )
 }
 
-userSchema.methods.AccessRefreshToken = function() {
+userSchema.methods.AccessRefreshToken = function () {
     return jwt.sign(
         {
-            _id: this._id
+            _id: this._id,
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
         }
     )
 }
